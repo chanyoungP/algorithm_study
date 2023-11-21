526c7e3c3330cfa42bc54fadd168b2e04835c934
박찬영 <chanchany0112@gmail.com>
2023-11-21 18:56:55 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.04 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -9,13 +9,13 @@ def solution(operations):
         if s =="I":
             q.append(v)
         elif s == "D" and len(q) > 0:
-            if v == 1:
+            if v == 1: # D이고 1이면 최댓값을 삭제
                 q.pop()
-            else:
+            else: # D 이고 -1이면 최솟값을 삭제 
                 q.pop(0)
-    if len(q) == 0:
+    if len(q) == 0: # 모든 명령을 수행하고 q가 비어있으면 0,0 을 리턴
         answer = [0,0]
-    else:
+    else: # 아닌 경우에는 max min 값을 리턴 
         answer = [max(q),min(q)]
 
             
